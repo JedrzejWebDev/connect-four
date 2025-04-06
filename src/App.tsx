84fa30b7aabacf996b2b17712182aad78bb3162d
[JedrzejWebDev] <[djsahiu@interia.pl]>
2025-04-06 14:07:03 +0200

    added-some-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
+import Modal from "react-modal";
 import "./App.css";
+import { BsInfoSquareFill } from "react-icons/bs";
+import { FaExpand, FaCompress } from "react-icons/fa";
+import { IoIosRefresh } from "react-icons/io";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [board, setBoard] = useState(
@@ -10,8 +15,26 @@ function App() {
   const [hoveredCol, setHoveredCol] = useState<number | null>(null);
   const [currentPlayer, setCurrentPlayer] = useState("R");
   const [gameOver, setGameOver] = useState(false);
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   const isTouchDevice = "ontouchstart" in window;
+
+  const toggleFullscreen = () => {
+    if (!isFullscreen) {
+      // Wejdź w tryb pełnoekranowy
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      }
+    } else {
+      // Wyjdź z trybu pełnoekranowego
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+
+    setIsFullscreen(!isFullscreen); // Przełącz stan fullscreen
+  };
 
   const getAvailableRow = (colIndex: number) => {
     for (let row = board.length - 1; row >= 0; row--) {
@@ -66,12 +89,8 @@ function App() {
       const detectedWinner = checkWinner(newBoard);
       if (detectedWinner) {
         setGameOver(true);
-        console.log(
-          detectedWinner === "Draw" ? "It's a draw!" : `${detectedWinner} wins!`
-        );
       } else if (isBoardFull(newBoard)) {
         setGameOver(true);
-        console.log("It's a draw!");
       } else {
         setCurrentPlayer(currentPlayer === "R" ? "Y" : "R");
       }
@@ -79,34 +98,112 @@ function App() {
   };
 
   return (
-    <div className="board">
-      {board.map((row, rowIndex) => (
-        <div key={rowIndex} className="row">
-          {row.map((cell, colIndex) => {
-            const availableRow = getAvailableRow(colIndex);
-            const isHighlight =
-              !isTouchDevice &&
-              hoveredCol === colIndex &&
-              availableRow === rowIndex;
-            return (
-              <div
-                key={colIndex}
-                className="cell"
-                onMouseEnter={
-                  !isTouchDevice ? () => setHoveredCol(colIndex) : undefined
-                }
-                onMouseLeave={
-                  !isTouchDevice ? () => setHoveredCol(null) : undefined
-                }
-                onClick={() => handleCellClick(colIndex)}
-              >
-                {isHighlight && <div className={`disc ${currentPlayer}`} />}
-                {cell && <div className={`disc ${cell}`} />}
-              </div>
-            );
-          })}
+    <div className="game-container">
+      <BsInfoSquareFill
+        className="info-icon"
+        onClick={() => setShowInstructions(true)}
+      />
+      <Modal
+        isOpen={showInstructions}
+        onRequestClose={() => setShowInstructions(false)}
+        contentLabel="Instrukcja do gry"
+        className="ModalContent"
+        overlayClassName="ModalOverlay"
+      >
+        <p>
+          <strong>Instrukcje gry Connect Four:</strong>
+        </p>
+        <ul>
+          <li>
+            <strong>Celem gry:</strong> Ułóż cztery swoje pionki w jednej linii
+            - poziomo, pionowo lub diagonalnie.
+          </li>
+          <li>
+            <strong>Rozpoczęcie gry:</strong> Gra toczy się na planszy o
+            wymiarach 7x6. Gracze na zmianę umieszczają swoje pionki w
+            kolumnach.
+          </li>
+          <li>
+            <strong>Przyjmowanie ruchów:</strong> W swojej turze gracz wybiera
+            jedną z siedmiu kolumn i wrzuca pionek na planszę. Pionek opada na
+            najniższą dostępną wolną pozycję w danej kolumnie.
+          </li>
+          <li>
+            <strong>Wygrać grę:</strong> Gracz, który pierwszy ułoży cztery
+            pionki w jednej linii (poziomo, pionowo lub na przekątnej), wygrywa
+            grę.
+          </li>
+          <li>
+            <strong>Remis:</strong> Jeśli plansza zostanie zapełniona, a żaden z
+            graczy nie ułoży czterech pionków w linii, gra kończy się remisem.
+          </li>
+          <li>
+            <strong>Strategia:</strong> Pamiętaj, aby blokować przeciwnika i
+            szukać okazji do stworzenia własnych linii czterech pionków.
+          </li>
+        </ul>
+        <p>Powodzenia i miłej zabawy!</p>
+        <button onClick={() => setShowInstructions(false)}>Zamknij</button>
+      </Modal>
+
+      <h1 className="game-title">Connect Four</h1>
+
+      <div
+        className="icon-to-zoom-in-or-out"
+        onClick={toggleFullscreen}
+        aria-label="Przełącz tryb pełnoekranowy"
+      >
+        {isFullscreen ? <FaCompress /> : <FaExpand />}
+      </div>
+
+      <div className="board">
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((cell, colIndex) => {
+              const availableRow = getAvailableRow(colIndex);
+              const isHighlight =
+                !gameOver &&
+                !isTouchDevice &&
+                hoveredCol === colIndex &&
+                availableRow === rowIndex;
+              return (
+                <div
+                  key={colIndex}
+                  className="cell"
+                  style={{ cursor: gameOver ? "default" : "pointer" }}
+                  onMouseEnter={
+                    !isTouchDevice && !gameOver
+                      ? () => setHoveredCol(colIndex)
+                      : undefined
+                  }
+                  onMouseLeave={
+                    !isTouchDevice && !gameOver
+                      ? () => setHoveredCol(null)
+                      : undefined
+                  }
+                  onClick={() => handleCellClick(colIndex)}
+                >
+                  {isHighlight && <div className={`disc ${currentPlayer}`} />}
+                  {cell && <div className={`disc ${cell}`} />}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+
+      <div className="game-status">
+        <span className="result-message">
+          {gameOver
+            ? isBoardFull(board)
+              ? "Remis!"
+              : `Gracz ${currentPlayer} wygrywa!`
+            : ""}
+        </span>
+        <div className="refresh-board">
+          {gameOver ? <IoIosRefresh className="refresh-icon"/> : null}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
